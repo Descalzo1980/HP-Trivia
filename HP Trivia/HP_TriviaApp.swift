fe7424c24edac88fde0d28757f3b141e5ee6e38a
Stas <ragioniere1980@gmail.com>
2025-09-01 18:53:56 +0300

    first commit

---
+++ HP Trivia/HP_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HP_TriviaApp.swift
+//  HP Trivia
+//
+//  Created by Станислав Леонов on 01.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct HP_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
